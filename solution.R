7979d65a5763d3901bfe32f9bff9ecffa02df42c
Haiman Syafiq <20b2114@ubd.edu.bn>
2023-09-24 17:15:29 +0800

    added backtracking file
    done by HiMan

--- solution.R
+++ solution.R
@@ -94,67 +94,6 @@ even_odd_avg_len <- collatz_df %>%
   group_by(parity) %>%
   summarise(avg_length = mean(length), sd_length = sd(length))
 
-
-## Task 3----------------------------------------------------------
-
-backtracks_df <- tibble()                   #Initializing backtrack dataframe
-
-for(j in 1:10000){
-  seqTemp <- collatz_df$seq[[j]]          # let seqTemp be the list of starting number j
-  
-  for(i in 1:length(seqTemp)){
-    if(seqTemp[1] >= seqTemp[i]){
-      next
-    } else if(seqTemp[1] < seqTemp[i]){
-      backtracks_df <- rbind(backtracks_df, collatz_df[j,])   # If seq backtracks, add row to backtracks_df then break loop
-      break
-    } else {
-      break       # if no backtracks, break loop
-    }
-  }
-}
-backtracks_df
-
-# Find the frequently occurring number of times they go above their starting
-
-backtracks_count <- backtracks_df %>%
-  mutate(count = 0)
-
-backtracks_count$count
-for (y in 1:length(backtracks_count$start)) {
-  seqCount <- backtracks_count$seq[[y]]
-  for(z in 1:length(seqCount)){
-    if(seqCount[z] > seqCount[1]){
-      backtracks_count$count[y] = backtracks_count$count[y] + 1
-    }
-  }
-}
-
-mode_backtrack <- backtracks_count %>% 
-  summarise(mode = Mode(count))
-
-mode_backtrack
-
-# maximum value reached after the first backtrack for these sequences
-
-max_after_backtrack <- backtracks_df %>%
-  mutate(max = 0)
-
-# even odd backtracks
-
-n_odd <- backtracks_df %>%
-  filter(Parity == "Odd") %>%
-  summarise(n_odd = n())
-n_odd
-
-n_even <- backtracks_df %>%
-  filter(Parity == "Even") %>%
-  summarise(n_even = n())
-n_even
-
-even_odd_backtrack <- tibble("Parity" = c("Odd", "Even"), "Freq" = c(n_odd[[1]], n_even[[1]]))
-even_odd_backtrack
-
 ## Task 4----------------------------------------------------------------
 
 # 1
