892184ce5eb1507b189d3a346ed8e3478c2949b3
Afiq-stack <20b2016@ubd.edu.bn>
2023-09-26 07:39:06 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -76,19 +76,4 @@ for(i in 1:10000){
 }
 
 #-------------------------------------------------------
-#task 2
-#top 10 starting integers
-top10longest <- collatz_df %>%
-  arrange(desc(length)) %>%
-  head(10)
-
-#The starting integers 
-max_val_int <- collatz_df %>%
-  arrange(desc(max_val)) %>%
-  slice(1)
-
-#average length and the standard devietion
-even_odd_avg_len <- collatz_df %>%
-  group_by(parity) %>%
-  summarise(avg_length = mean(length), sd_length = sd(length))
 
