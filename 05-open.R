c20e6dbe83415f7d160bbf4a224b067602ec0329
Lim311 <141397456+Lim311@users.noreply.github.com>
2023-09-25 22:43:06 +0800

    Update 05-open.R

--- 05-open.R
+++ 05-open.R
@@ -1,21 +1,31 @@
-<sub>The question is "What are the most common stopping times, the number of steps it takes for a sequence to first go below its starting integer?"
-to achieve this,we can :
-1. Calculating the stopping time for each Collatz sequence.
-2. Creating a histogram or frequency distribution of stopping times to identify the most common stopping times.
-3. Providing a clear and understandable summary of your findings in your script.
+#example for task 5
+library(dplyr)
+library(ggplot2)
 
-**Process**
 
-Calculating Stopping Times: We calculate the stopping time for each Collatz sequence by counting the number of iterations it takes for the sequence to reach a value less than the starting integer and eventually converge to 1.
-Frequency Distribution: After calculating stopping times for a range of starting integers (e.g., from 1 to 10,000), we create a histogram or frequency distribution of these stopping times. Each bar in the histogram represents a range of stopping times (e.g., 1-5, 6-10, etc.).
-Identifying Common Stopping Times: We identify the most common stopping times by finding the peak(s) in the frequency distribution. These peaks correspond to the stopping times that occur most frequently across the Collatz sequences.
+# Calculate the stopping time for each sequence
+collatz_df <- collatz_df %>%
+  mutate(stopping_time = sapply(seq, function(x) {
+    index <- which(x < start)[1]  # Find the index where the sequence goes below the start
+    if (is.na(index)) {
+      return(length(x))  # If it never goes below, use the length of the sequence
+    } else {
+      return(index)  # Otherwise, use the index
+    }
+  }))
 
-**Methodologies**
+# The most common stopping times
+common_stopping_times <- collatz_df %>%
+  group_by(stopping_time) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
 
-The finding is that after performing the analysis, we discover that the most common stopping times in Collatz sequences are often clustered around certain values.
-For example, we may find that stopping times of 3, 7, and 15 occur with high frequency. 
-This suggests that there are certain "stopping time patterns" that are prevalent in Collatz sequences.
-
-**Reference:**
-Collatz, L. (1937). Über die Vermutung, die sich aus der Collatzschen Vermutung ergibt. German Mathematical Society.
-Lagarias, J. C. (1985). The 3x + 1 Problem and Its Generalizations. American Mathematical Monthly, 92(1), 3-23.<sub>
+# Visualize the most common stopping times
+ggplot(common_stopping_times, aes(x = stopping_time, y = count)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Most Common Stopping Times in Collatz Sequences",
+    x = "Stopping Time",
+    y = "Frequency"
+  ) +
+  theme_minimal()
