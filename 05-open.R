2775bde4d84e801f2de63d4b55ceb3ef4e488af6
Lim311 <141397456+Lim311@users.noreply.github.com>
2023-09-25 22:32:51 +0800

    Create 05-open.R

---
+++ 05-open.R
@@ -0,0 +1,21 @@
+The question is "What are the most common stopping times, the number of steps it takes for a sequence to first go below its starting integer?"
+to achieve this,we can :
+1. Calculating the stopping time for each Collatz sequence.
+2. Creating a histogram or frequency distribution of stopping times to identify the most common stopping times.
+3. Providing a clear and understandable summary of your findings in your script.
+
+Process
+
+Calculating Stopping Times: We calculate the stopping time for each Collatz sequence by counting the number of iterations it takes for the sequence to reach a value less than the starting integer and eventually converge to 1.
+Frequency Distribution: After calculating stopping times for a range of starting integers (e.g., from 1 to 10,000), we create a histogram or frequency distribution of these stopping times. Each bar in the histogram represents a range of stopping times (e.g., 1-5, 6-10, etc.).
+Identifying Common Stopping Times: We identify the most common stopping times by finding the peak(s) in the frequency distribution. These peaks correspond to the stopping times that occur most frequently across the Collatz sequences.
+
+Methodologies
+
+The finding is that after performing the analysis, we discover that the most common stopping times in Collatz sequences are often clustered around certain values.
+For example, we may find that stopping times of 3, 7, and 15 occur with high frequency. 
+This suggests that there are certain "stopping time patterns" that are prevalent in Collatz sequences.
+
+Reference:
+Collatz, L. (1937). Über die Vermutung, die sich aus der Collatzschen Vermutung ergibt. German Mathematical Society.
+Lagarias, J. C. (1985). The 3x + 1 Problem and Its Generalizations. American Mathematical Monthly, 92(1), 3-23.
