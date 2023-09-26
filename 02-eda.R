fb52d53fe8479617f6465fdc6feae3ee713d9668
Afiq-stack <20b2016@ubd.edu.bn>
2023-09-26 08:44:24 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -1,14 +1,20 @@
+library(dplyr)
+
 #-------------------------------------------------------
 #task 2
 #top 10 starting integers
-top10longest <- collatz_df[3] %>%
+top10longest <- collatz_df %>%
   arrange(desc(Length)) %>%
   head(10)
+  
+top10longest <- top10longest$start
 
 #The starting integers 
 max_val_int <- collatz_df %>%
   arrange(desc(max_val)) %>%
   slice(1)
+
+max_val_int <- pull(max_val_int, max_val)
 
 #average length and the standard devietion
 even_odd_avg_len <- collatz_df %>%
