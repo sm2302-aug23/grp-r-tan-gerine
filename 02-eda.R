55b7ecd9477307dd83e8ee730f3a63a10d63dc66
Afiq-stack <20b2016@ubd.edu.bn>
2023-09-26 09:14:35 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -14,10 +14,14 @@ max_val_int <- collatz_df %>%
   arrange(desc(max_val)) %>%
   slice(1)
 
-max_val_int <- pull(max_val_int, max_val)
+max_val_int <- pull(max_val_int, start)
 
 #average length and the standard devietion
-even_odd_avg_len <- collatz_df %>%
+even_odd_avg_len_sd <- collatz_df %>%
   mutate(even_odd = ifelse(start %% 2 == 0, "Even", "Odd")) %>%
   group_by(even_odd) %>%
   summarise(avg_length = mean(Length), sd_length = sd(Length))
+
+even_odd_avg_len <- even_odd_avg_len_sd$avg_length
+
+even_odd_sd_len <- even_odd_avg_len_sd$sd_length
